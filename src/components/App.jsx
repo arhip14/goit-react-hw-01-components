b78f7c419ab57e399c8cea08788163d471d3ef79
arhip14 <kruvdyk1503@gmail.com>
2023-08-06 16:47:42 +0300

    initial-commit 10

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import Profile from './profile';
-import Statistics from './statistic/statistics';
+// import Statistics from './statistic/statistics';
 import FriendList from './friendsList';
 import TransactionHistory from './transaction/transactionHistory';
 import user from '../json-info/user.json';
-import data from '../json-info/data.json';
+// import data from '../json-info/data.json';
 import friends from '../json-info/friends.json';
 import transactions from '../json-info/transactions.json'; 
 import '../index.css';
@@ -13,10 +13,11 @@ export const App = () => {
   return (
     <div className="app">
       <Profile {...user} />
-      <Statistics title="Upload stats" stats={data} />
+      {/* <Statistics title="Upload stats" stats={data} /> */}
       <FriendList friends={friends} />
       <TransactionHistory items={transactions} /> {}
     </div>
   );
 };
+
 export default App;
